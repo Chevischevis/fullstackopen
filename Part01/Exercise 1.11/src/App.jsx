015b77f0a995c7bf20c1269cff6de7b14673d0f8
Chevischevis <chevischevis@ciencias.unam.mx>
2025-08-22 20:29:21 -0600

    Estado del componente y controladores de eventos

--- Part01/Exercise 1.11/src/App.jsx
+++ Part01/Exercise 1.11/src/App.jsx
@@ -1,35 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Hello = ({ name, age }) => {
+  //Desectructuracion
+  // const name = props.name
+  // const age = props.age
+  // const { name, age } = props
+  const bornYear = () => new Date().getFullYear - props.age
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
       </p>
-    </>
+      <p> So you were probably born in {bornYear()}</p>
+    </div>
   )
 }
 
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('App is rendering with counter value: ', counter)
+  //Usando useState, para recordar el valor del contador
+  // setTimeout(() => setCounter(counter + 1), 1000)
+
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  } 
+  const setToZero = () =>{
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+  const decreaseByOne = () =>{
+console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  } 
+  // const name = 'Peter'
+  // const age = 10
+
+  // return (
+  //   <div>
+  //     <h1>Greetings</h1>
+  //     <Hello name="Maya" age={26 + 10} />
+  //     <Hello name={name} age={age} />
+  //   </div>
+  // )
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
+
+  const Display = (props) =><div>{props.counter}</div>
+  const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+  return (
+    <>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+      {/* <button onClick={handleClick}>Click</button> */}
+    </>
+  )
+}
+//Comentario importante
+//Desectructuracion
 export default App
--- Part01/Exercise 1.11/src/main.jsx
+++ Part01/Exercise 1.11/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import App from './App'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import ReactDOM from 'react-dom/client'
+import App from './App'
+let counter = 1
+// const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+// }
+// setInterval(() => {
+//   refresh()
+//   counter += 1
+// }, 1000)
