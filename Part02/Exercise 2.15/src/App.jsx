4e6e42a9831e38cc54570d408267987cdc98474f
Chevischevis <chevischevis@ciencias.unam.mx>
2025-08-24 22:23:36 -0600

    Ejercicio 2.8

--- Part02/Exercise 2.15/src/App.jsx
+++ Part02/Exercise 2.15/src/App.jsx
@@ -2,27 +2,58 @@ import Note from './Components/Note'
 import { useState } from 'react'
 
 const App = () => {
-const  [persons, setPersons] = useState([{name: 'Arto Hellas'}])
-const [NewName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-return (
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(newName + `is already added to phonebook`)
+      return
+    }
+    const personObject = {
+      name: newName}
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  const addNumber = (event) => {
+    event.preventDefault()
+    const numberObject = {
+      number: newNumber}
+    setPersons(persons.concat(numberObject))
+    setNewNumber('')
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  console.log(persons.map(person => person.name))
+  return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit"></button>
-          </div>
-          </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => <Note key={person.name} note={person} />)}
-      </ul>
       <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName}
+                    onChange={handleNameChange}/>
+        </div>
+        <div>number: <input value={newNumber}
+                      onChange={handleNumberChange}/></div>
+        <div>
+          <button type="submit"> save</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person => (
+          <p key={person.name}  >{person.name}</p>
+        ))}
     </div>
-  ) 
+  )
 }
 
 export default App
