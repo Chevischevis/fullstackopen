82eabbdd91c96b3cb372a1e64feda5fc3c6a5d0e
Chevischevis <chevischevis@ciencias.unam.mx>
2025-08-24 21:31:24 -0600

    Me faltaron documentode para la pagina de notas

--- Part02/Exercise 2.15/src/App.jsx
+++ Part02/Exercise 2.15/src/App.jsx
@@ -1,35 +1,28 @@
 import Note from './Components/Note'
 import { useState } from 'react'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
-const addNote = (event) => {
-  event.preventDefault()
-  const noteObject = {
-    content: newNote,
-    important: Math.random() < 0.5,
-    id: notes.length + 1,
-  }
+const App = () => {
+const  [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+const [NewName, setNewName] = useState('')
 
-  setNotes(notes.concat(noteObject))
-  setNewNote('')
-}
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-  return (
+return (
     <div>
-      <h1>Notes</h1>
-      <ul> {notes.map(note => <Note key={note.id} note={note}></Note>)}</ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit"></button>
+          </div>
+          </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <Note key={person.name} note={person} />)}
+      </ul>
+      <div>debug: {newName}</div>
     </div>
-  )
+  ) 
 }
 
 export default App
