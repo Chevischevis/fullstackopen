a8b881842d0f8207fed926c78c33fdc05f079e77
Chevischevis <chevischevis@ciencias.unam.mx>
2025-09-06 09:21:08 -0600

    merger con la rama remta

--- Part02/Exercise 2.15/src/App.jsx
+++ Part02/Exercise 2.15/src/App.jsx
@@ -13,30 +13,58 @@ import {PhoneBook, PersonForm, Persons} from './Components/PhoneBook'
 import { useState, useEffect} from 'react'
 import axios from 'axios' 
 import './App.css'
+useEffect(() => {
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })  
+  }, [])
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-  useEffect(() => {
-    fetch('http://localhost:3001/persons')
-      .then(response => response.json())
-      .then(data => setPersons(data))
-  }, [])
-  console.log('render', persons.length, 'persons')
->>>>>>> 2f6241b (instalacion de axios al ejercicio)
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(newName + `is already added to phonebook`)
+      return
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
-  // ...
+  const ShowInformation = filter ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())) : persons
+  console.log(persons.map(person => person.name))
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <PhoneBook filter={filter} handleFilterChange={handleFilterChange} />
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} 
+                  handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>  
+      
+      <h2>Numbers</h2>
+     <Persons ShowInformation={ShowInformation} />
+    </div>
+  )
 }
+
+export default App
