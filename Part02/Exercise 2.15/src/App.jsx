4e95b3f3f881180a68a4ad0258968bd00ff53b0e
Chevischevis <chevischevis@ciencias.unam.mx>
2025-09-06 09:06:27 -0600

    Agenda Telefonica con el pull del remoto

--- Part02/Exercise 2.15/src/App.jsx
+++ Part02/Exercise 2.15/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
@@ -7,6 +8,23 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+=======
+import {PhoneBook, PersonForm, Persons} from './Components/PhoneBook'
+import { useState, useEffect} from 'react'
+import axios from 'axios' 
+import './App.css'
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+      .then(response => response.json())
+      .then(data => setPersons(data))
+  }, [])
+  console.log('render', persons.length, 'persons')
+>>>>>>> 2f6241b (instalacion de axios al ejercicio)
 
 
   useEffect(() => {
